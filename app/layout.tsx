3ce9582cbf3b7a1b2d125d32e27a7bbab7a4742a
Om Shejul <contact@omshejul.com>
2024-05-13 23:13:50 +0530

    chore: Update package version and add version display in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
+ import packageData from '../package.json'
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+        <div className="pill z-50 text-gray-500 absolute bottom-0 right-0 m-4 ">
+          v{packageData.version}
+        </div>
+      </body>
     </html>
   );
 }
