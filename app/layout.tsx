62afe52abf12fccdfceb8712f51507e86cb567e1
Om Shejul <contact@omshejul.com>
2024-05-17 13:14:41 +0530

    chore: Update package version and add email icon to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "./globals.css";
  import packageData from '../package.json'
+import Email from "./Icons/Email"
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -19,6 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         {children}
+        <a href="https://omshejul.com/qr/" className="pill z-50 border rounded-full border-[hsla(0,0%,50%,0.5)] p-2 text-gray-500 absolute bottom-0 left-0 m-4 ">
+          <Email />
+        </a>
         <div className="pill z-50 text-gray-500 absolute bottom-0 right-0 m-4 ">
           v{packageData.version}
         </div>
